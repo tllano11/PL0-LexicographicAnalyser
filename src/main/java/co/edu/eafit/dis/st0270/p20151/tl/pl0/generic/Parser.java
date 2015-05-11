4770cb9121ad2fe89c0e4d2384c35a5470373197
tllanos <tllanos@f257ceec-0917-4462-8bce-99dca8b42489>
2015-05-11 02:04:49 +0000

    instruction() method added on Parser.java, changed .compare(...) to .equals(...), REMARK: Maven won't compile

--- src/main/java/co/edu/eafit/dis/st0270/p20151/tl/pl0/generic/Parser.java
+++ src/main/java/co/edu/eafit/dis/st0270/p20151/tl/pl0/generic/Parser.java
@@ -25,7 +25,7 @@ public class Parser {
     private void reset() {
         state = 1;
 
-	try {
+        try {
             token = lexer.getToken();
         } catch (IOException ioe) {
             System.err.println("Error: " + ioe);
@@ -36,10 +36,10 @@ public class Parser {
     public void analyze (GenericLexer lexer) throws ParserException {
         this.lexer = lexer;
         reset();
-	
-	try {
-	    program();
-	} catch (IOException ioe) {
+        
+        try {
+            program();
+        } catch (IOException ioe) {
             System.err.println("Error: " + ioe);
             System.exit(1);
         }
@@ -47,74 +47,130 @@ public class Parser {
     }
 
     private void program () throws ParserException {
-	block();
-	if (!token.getLex.compare(EOF)) throw new ParserException(token);
+        block();
+        if (!token.getLex.equals(EOF)) throw new ParserException(token);
     }
 
     private void block () throws ParserException {
-	defConst();
-	defVar();
-	defProc();
-	instruction();
+        defConst();
+        defVar();
+        defProc();
+        instruction();
     }
 
     private void defConst () throws ParserException {
-	if (token.getLex().compare("const")) {
-	    token = lexer.getToken();
-	    boolean var = false;
-	    
+        if (token.getLex().equals("const")) {
+            token = lexer.getToken();
+            boolean var = false;
+            
             while (!var) {
-		if (token.getType == 4) {
-		    token = lexer.getToken();
-		    if (!token.getLex().compare(":")) throw new ParserException(token);
+                if (token.getType == 4) {
+                    token = lexer.getToken();
+                    if (!token.getLex().equals(":")) throw new ParserException(token);
 
-		    token = lexer.getToken();
-		    if (token.getType != 5) throw new ParserException(token);
+                    token = lexer.getToken();
+                    if (token.getType != 5) throw new ParserException(token);
 
-		    token = lexer.getToken();
-		    if (token.getLex().compare(";")) var = true;
-		    else if (!token.getLex().compare(",")) throw new ParserException(token);
-		    else token = lexer.getToken();
-		} else {
-		    throw new ParserException(token); 
-		}
-	    }
-	}
+                    token = lexer.getToken();
+                    if (token.getLex().equals(";")) var = true;
+                    else if (!token.getLex().equals(",")) throw new ParserException(token);
+                    else token = lexer.getToken();
+                } else {
+                    throw new ParserException(token); 
+                }
+            }
+        }
     }
 
     private void defVar() throws ParserException {
-	if (token.getLex().compare("var")) {
-	    token = lexer.getToken();
-	    boolean var = false;
+        if (token.getLex().equals("var")) {
+            token = lexer.getToken();
+            boolean var = false;
 
-	    while (!var) {
-		if (token.getType == 4) {
-		    token = lexer.getToken();
-		    if (token.getLex().compare(";")) var = true;
-		    else if (!token.getLex().compare(",")) throw new ParserException(token);
-		    else token = lexer.getToken();
-		} else {
-		    throw new ParserException(token); 
-		}
-	    }
-	}
+            while (!var) {
+                if (token.getType == 4) {
+                    token = lexer.getToken();
+                    if (token.getLex().equals(";")) var = true;
+                    else if (!token.getLex().equals(",")) throw new ParserException(token);
+                    else token = lexer.getToken();
+                } else {
+                    throw new ParserException(token); 
+                }
+            }
+        }
     }
 
     private void defProc() throws ParserException {
-	if (token.getLex().compare("procedure")) {
-	    token = lexer.getToken();
-	    if (token.getType == 4) {
-	        token = lexer.getToken();
-	        if (token.getLex().compare(";")) block();
-		if (!token.getLex().compare(";")) throw new ParserException(token);
-	    } else {
-		    throw new ParserException(token);
-	    }
+        if (token.getLex().equals("procedure")) {
+            token = lexer.getToken();
+            if (token.getType == 4) {
+                token = lexer.getToken();
+                if (token.getLex().equals(";")) block();
+                if (!token.getLex().equals(";")) throw new ParserException(token);
+            } else {
+                    throw new ParserException(token);
+            }
+        }
+    }
+
+    private void instruction() throws ParserException{
+        if (token.getType() == 4){
+            token = lexer.getToken();
+            if (token.getLex().equals(":=")) {
+                token = lexer.getToken();
+                expr();
+            } else {
+                throw new ParserException(token);
+            }
+        } else if(token.getLex().equals("call")){
+            token = lexer.getToken();
+            if (token.getType() != 4){
+                throw new ParserException(token);
+            }
+        } else if (token.getLex().equals("begin")){
+            token = lexer.getToken();
+            instruction();
+            token = lexer.getToken();
+            while (!token.getLex().equals("end")){
+                if (token.getLex().equals(";")){
+                    token = lexer.getToken();
+                    instruction();
+                } else {
+                    throw new ParserException(token);
+                }
+            }       
+        } else if(token.getLex().equals("if")){
+            token = lexer.getToken();
+            condition();
+            token = lexer.getToken();
+            if (token.getLex().equals("then")){
+                token = lexer.getToken();
+                instruction();
+            } else{
+                throw new ParserException(token);
+            }
+        }  else if(token.getLex().equals("while")){
+            token = lexer.getToken();
+            condition();
+            token = lexer.getToken();
+            if (token.getLex().equals("do")){
+                token = lexer.getToken();
+                instruction();
+            } else{
+                throw new ParserException(token);
+            }
+        } 
+    }
+
+    private void condition() throws ParserException {
+        if(token.getLex().equals("odd")){
+            token = lexer.getToken();
+            expr();
 	}
     }
 
     private void expr() throws ParserException {
-	//Here we check if the following tokens form a expression
-	 RestExpr(term());
+        //Here we check if the following tokens form a expression
+	//RestExpr(term());
     }    
 }
