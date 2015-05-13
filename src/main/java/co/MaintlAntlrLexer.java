004e3aa697c9a0149314fca0b77b8c3ae9eff517
Luis Miguel Mejía Suárez <luis.miguel.mejia@hotmail.com>
2015-05-13 13:49:54 +0000

    Falta LexerError y JFlex_EOF

--- src/main/java/co/MaintlAntlrLexer.java
+++ src/main/java/co/MaintlAntlrLexer.java
@@ -9,6 +9,30 @@ import co.edu.eafit.dis.st0270.p20151.tl.pl0.generic.ParserException;
 import java.io.IOException;
 
 public class MaintlAntlrLexer {
+
+    private void list (GenericLexer lexer) {
+        try {
+            Generic Token t = lexer.nextToken();
+            Token eof = lexer.getEOF();
+
+            while(t.getType() != eof.getType()) {
+                switch (t.getType()) {
+                    case 6:
+                        //If is a white space, ignore
+                        break;
+                    default:
+                        System.out.println("tipo: " + t.getType() +
+                                           " valor: " + t.getLex() +
+                                           " fila: " + t.getLine() +
+                                           " col: " + t.getCol);
+                        break;
+                }
+                
+                t = lexer.nextToken();
+        } catch (IOException ex) {
+                System.err.println("Error: " + ex.getMessage());       
+        }
+    }
     
     public static void main (String args[]) {
         if (args.length != 1) {
@@ -18,17 +42,18 @@ public class MaintlAntlrLexer {
 
         ANTLRFileStream afs = null;
         GenericLexer lexer = null;
-	Parser parser = new Parser();
+        //Parser parser = new Parser();
 
-	for (int i = 0; i <  args.length; i++) {
+        for (int i = 0; i <  args.length; i++) {
             try {
                 afs = new ANTLRFileStream(args[i]);
                 lexer = new GenericLexer(new tlAntlrLexer(afs));
-                parser.analyze(lexer);
-            } catch (IOException | ParserException ex) {
+                list(lexer);
+                //parser.analyze(lexer)
+            } catch (IOException ex) {
                 System.err.println("Error: " + ex.getMessage());
                 System.exit(1);
-	    }
-	} 
+            }
+        } 
     }
 }
